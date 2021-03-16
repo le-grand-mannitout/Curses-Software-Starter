85835e1499e75a588e55f6cacc4ec54ee45ec807
le-grand-mannitout <ouaksel.madil@gmail.com>
2021-03-16 20:09:24 +0100

    Change cursor type by background color, remove default one and patch executing task function

--- interface.py
+++ interface.py
@@ -10,8 +10,21 @@ def display_moods(stdscr, mood_names: list):
     """
         Display moods names on the curses window
     """
+    stdscr.addstr(0, 0, "--Mood selector--")
     for i, mood in enumerate(mood_names):
-        stdscr.addstr(i*2, 2, mood)
+        stdscr.addstr(i*2 + 2, 2, mood)
+
+
+def cursor_mov(stdscr,
+               mood_names: list,
+               index_to_cursor: int):
+    """
+        Move the cursor by changing text background color
+    """
+    stdscr.addstr(index_to_cursor,
+                  2,
+                  mood_names[(index_to_cursor - 2)//2],
+                  curses.color_pair(2))
 
 
 def graphical_interface(stdscr,
@@ -21,13 +34,15 @@ def graphical_interface(stdscr,
     """
     stdscr.clear()
     curses.init_pair(1, curses.COLOR_YELLOW, curses.COLOR_BLACK)
+    curses.init_pair(2, curses.COLOR_BLACK, curses.COLOR_YELLOW)
     stdscr.attron(curses.color_pair(1))
+    curses.curs_set(0)
 
     mood_names = [mood for mood in mood_dict]
 
     display_moods(stdscr, mood_names)
 
-    k, cursor = 0, 0
+    k, cursor = 0, 2
     height, width = stdscr.getmaxyx()
 
     while 1:
@@ -39,12 +54,13 @@ def graphical_interface(stdscr,
             cursor -= 2
 
         elif k == KEY_ENTER:
-            return mood_dict[mood_names[cursor//2]]
+            return mood_dict[mood_names[(cursor - 2)//2]]
 
-        cursor = max(0, cursor)
-        cursor = min((len(mood_names)*2) - 2, cursor)
+        cursor = max(2, cursor)
+        cursor = min(len(mood_names)*2, cursor)
 
-        stdscr.move(cursor, 0)
+        display_moods(stdscr, mood_names)
+        cursor_mov(stdscr, mood_names, cursor)
 
         stdscr.refresh()
         k = stdscr.getch()
